a2c091f17f90a0d82dce4b72db1c2d7564db98b0
Josh Elias <jmelias317@gmail.com>
2022-10-06 13:03:52 -0400

    t

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { collection } = require("../model/user");
 
 const URL =
-  "mongodb+srv://jmelias13:admin@testtarnopol.ipp4ztg.mongodb.net/?retryWrites=true&w=majority/tarnopolfootball";
+  "mongodb+srv://jmelias13:admin@testtarnopol.ipp4ztg.mongodb.net/tarnopolFootball?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
