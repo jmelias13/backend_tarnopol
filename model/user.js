f026f4bd3846c4db01b3e490f2fa96bdbafdebaa
jmelias13 <51060511+jmelias13@users.noreply.github.com>
2023-02-25 18:38:40 -0500

    Update user.js

--- model/user.js
+++ model/user.js
@@ -5,18 +5,6 @@ const userSchema = mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   role: { type: String, required: true },
-  picks: [
-    {
-      dateSubmitted: Date,
-      pick1: String,
-      pick2: String,
-      pick3: String,
-      pick4: String,
-      active: Boolean,
-      thursday_editable: Boolean,
-      sunday_editable: Boolean,
-    },
-  ],
   dateCreated: { type: String, required: true },
 });
 
